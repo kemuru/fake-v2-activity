38159ced6b64bfa61eec57391fda42a919ed2eff
marino <102478601+kemuru@users.noreply.github.com>
2023-03-10 03:10:55 +0100

    feat: script approves 2 wallets and stakes PNK

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,14 @@ import pnkJson from "./contracts/PNK.json"
 dotenv.config()
 
 const main = async () => {
+  // setup provider, 2 testing wallets, and the contracts to interact with
   const provider = new ethers.JsonRpcProvider(process.env.ARBITRUM_GOERLI_RPC)
   const firstWallet = new ethers.Wallet(
     process.env.PRIVATE_KEY_WALLET_1 as string,
+    provider
+  )
+  const secondWallet = new ethers.Wallet(
+    process.env.PRIVATE_KEY_WALLET_2 as string,
     provider
   )
   const pnkContract = new ethers.Contract(
@@ -21,24 +26,46 @@ const main = async () => {
     provider
   ) as any
 
-  // const functionArgs = [0]
+  // see courts, use this function to gather some data about some subcourt
+
+  // const functionArgs = [1]
   // const resultCourtsTx = await klerosCoreContract
   //   .connect(firstWallet)
   //   ["courts"](...functionArgs)
-  // console.log(resultCourtsTx)
-  // const approvePNKFunctionArgs = [process.env.WALLET_1, "200000000000000000000"]
-  // const resultApprovePNK = await pnkContract
+  //   console.log(resultCourtsTx)
+
+  //approve PNK on firstWallet and secondWallet
+
+  const approvePNKFunctionArgs = [
+    process.env.KLEROS_CORE_CONTRACT_ADDRESS,
+    "200000000000000000000",
+  ]
+  // const firstWalletResultApprovePNK = await pnkContract
   //   .connect(firstWallet)
   //   ["approve"](...approvePNKFunctionArgs)
-  // await resultApprovePNK
-  const setStakeFunctionArgs = [0, "1"]
-  const setStakeTransaction = await klerosCoreContract
-    .connect(firstWallet)
-    ["setStake"](...setStakeFunctionArgs, { gasLimit: 300000})
-    const txReceipt = await setStakeTransaction.wait();
-  console.log(txReceipt)
 
-  // resultMinStake.map((v: number) => { console.log(v.toString() )})
+  const secondWalletResultApprovePNK = await pnkContract
+    .connect(secondWallet)
+    ["approve"](...approvePNKFunctionArgs)
+  // await firstWalletResultApprovePNK.wait()
+  await secondWalletResultApprovePNK.wait()
+
+  //setStake of 200 PNK each on firstWallet and secondWallet
+
+  const setStakeFunctionArgs = [1, "200000000000000000000"]
+
+  // const firstWalletSetStakeTransaction = await klerosCoreContract
+  //   .connect(firstWallet)
+  //   ["setStake"](...setStakeFunctionArgs, { gasLimit: 500000 })
+
+  const secondWalletSetStakeTransaction = await klerosCoreContract
+    .connect(secondWallet)
+    ["setStake"](...setStakeFunctionArgs, { gasLimit: 500000 })
+
+  // const firstWalletTxReceipt = await firstWalletSetStakeTransaction.wait()
+  const secondWalletTxReceipt = await secondWalletSetStakeTransaction.wait()
+  // console.log(firstWalletTxReceipt)
+  console.log(secondWalletTxReceipt)
 }
 
 // executes script
